dbaf38451ffb4e151e5783ab73c98442cb7170ab
My Name <om20082003@gmail.com>
2023-10-19 15:30:20 +0530

    sadad

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const express  = require('express');
 // es module fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 
